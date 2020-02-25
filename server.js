462a0d4d5ae701800bc97a426a9cfef3e8932fc4
Noah Gibson <noah.gibson99@gmail.com>
2020-02-25 14:22:50 -0700

    Add post endpoint and cache middleware

--- server.js
+++ server.js
@@ -11,10 +11,31 @@ const client = redis.createClient(REDIS_PORT);
 const app = express();
 app.use(cors());
 
-app.post('/coordinates', async (req, res) => {
+app.post('/coordinates', cacheMiddleware, async (req, res) => {
   try {
     const response = await fetch(`https://global.skyhookwireless.com/wps2/json/location?key=${process.env.LOCATION_KEY}&user=${process.env.LOCATION_USER}`);
+    const location = await response.json();
+
+    client.setex('location', 3600, JSON.stringify(location));
   } catch (error) {
     console.error(error);
   }
+});
+
+function cacheMiddleware(req, res, next) {
+  client.get('location', (error, location) => {
+    if(error) {
+      throw error;
+    };
+
+    if(location !== null) {
+      res.send(location);
+    } else {
+      next();
+    }
+  });
+};
+
+app.listen(3001, () => {
+  console.log(`App listening on port ${PORT}`);
 });
