c177adcb9a6381690b67ef957bdffa9974fad36e
Noah Gibson <noah.gibson99@gmail.com>
2020-02-25 15:16:00 -0700

    Fix bug regarding fetch returning 400

--- server.js
+++ server.js
@@ -16,7 +16,19 @@ app.use(cors());
 
 app.post('/coordinates', cacheMiddleware, async (req, res) => {
   try {
-    const response = await fetch(`https://global.skyhookwireless.com/wps2/json/location?key=${process.env.LOCATION_KEY}&user=${process.env.LOCATION_USER}`);
+    const body = {
+      "date": `${new Date().getFullYear()}/${new Date().getMonth() + 1}/${new Date().getDate()}`,
+    };
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      redirect: 'follow'
+    };
+
+    const response = await fetch(`https://global.skyhookwireless.com/wps2/json/location?key=${process.env.LOCATION_KEY}&user=${process.env.LOCATION_USER}`, options);
     const location = await response.json();
 
     client.setex('location', 3600, JSON.stringify(location));
